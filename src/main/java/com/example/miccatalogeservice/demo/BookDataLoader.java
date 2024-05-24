f8c85f3f1b1017d03cb030ff1363347add062d9d
Lyova Hakobyan <lyov123lyov@gmail.com>
2024-05-24 14:54:25 +0400

    Update

--- src/main/java/com/example/miccatalogeservice/demo/BookDataLoader.java
+++ src/main/java/com/example/miccatalogeservice/demo/BookDataLoader.java
@@ -15,13 +15,13 @@ public class BookDataLoader {
 
     private final BookRepository bookRepository;
 
-//    @EventListener(ApplicationReadyEvent.class)
-//    public void addData() {
-//        var book1 = Book.of("123456", "Title1", "Author1", 123.123);
-//        var book2 = Book.of("456789", "Title2", "Author2", 456.456);
-//        var book3 = Book.of("789123", "Title3", "Author3", 789.789);
-//        bookRepository.save(book1);
-//        bookRepository.save(book2);
-//        bookRepository.save(book3);
-//    }
+    @EventListener(ApplicationReadyEvent.class)
+    public void addData() {
+        var book1 = Book.of("123456", "Title1", "Author1", 123.123);
+        var book2 = Book.of("456789", "Title2", "Author2", 456.456);
+        var book3 = Book.of("789123", "Title3", "Author3", 789.789);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+        bookRepository.save(book3);
+    }
 }
